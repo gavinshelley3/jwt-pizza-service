c4b92b6575b6034abfeedb89727a15e17ca30fc7
Gavin Shelley <gavinshelley3@gmail.com>
2026-01-27 16:02:52 -0700

    Add Service Test

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: { globals: globals.jest },
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: { react: { version: "18.0" } },
+  },
+]);
